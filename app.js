21f03733ecc545f0e30e8b702f0610f7a02b2ae2
ShaileshDsce <74795973+ShaileshDeveloper@users.noreply.github.com>
2020-11-29 19:49:15 +0530

    input and output

---
+++ app.js
@@ -0,0 +1,14 @@
+var btnTranslator = document.querySelector("#btn-translate");
+var textInput = document.querySelector("#txt-input")
+
+
+btnTranslator.addEventListener("click" ,  clickFunction);
+
+function clickFunction(){
+    console.log("shailesh");
+    console.log("input text is:" , textInput.value)
+    
+}
+    
+
+
